ccfd6568fccb482ffe3169055aabd56e8a19e267
Ismar Slomic <ismar@slomic.no>
2022-06-10 00:24:11 +0200

    feat(#7): validate at startup that the correct client secret file is being used - bump base image version to node:18.3.0-alpine3.16 - bump google-auth-library tp v8.0.2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:17.8.0-alpine3.15
+FROM node:18.3.0-alpine3.16
 
 LABEL maintainer="Ismar Slomic <ismar@slomic.no>"
 
--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const destroyer = require('server-destroy');
 const path = require('path');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-const clientSecret = require('/usr/src/config/client_secret.json');
+const clientSecretPath = process.env.CLIENT_SECRET || '/usr/src/config/client_secret.json';
+let clientSecret;
 const savedTokensPath = '/usr/src/config/tokens.json';
 
 async function main() {
+  validateClientSecretFile();
+  clientSecret = require(clientSecretPath);
   const oAuth2Client = await getAuthenticatedClient();
   await oAuth2Client.getTokenInfo(oAuth2Client.credentials.access_token);
 }
@@ -90,6 +93,51 @@ function getAuthenticatedClient() {
     });
     destroyer(server);
   });
+}
+
+const checkFileExistsSync = (filepath) => {
+  let exists = true;
+
+  try {
+    fs.accessSync(filepath, fs.constants.F_OK);
+  } catch (e) {
+    exists = false;
+  }
+
+  return exists;
+};
+
+const exitAndLogError = (errorMsg) => {
+  console.log(`[ERROR] ${errorMsg}`)
+  process.exit(1)
+}
+
+const readJsonFile = (filepath) => {
+  try {
+    return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  } catch (e) {
+    console.log(e);
+    exitAndLogError(`Failed reading JSON file at path ${filepath}`)
+  }
+}
+
+const validateClientSecretFile = () => {
+  if (!checkFileExistsSync(clientSecretPath)) {
+    exitAndLogError(
+        `Client Secret file at path '${clientSecretPath}' does not exist.`)
+  }
+
+  const secretFileContent = readJsonFile(clientSecretPath).installed;
+
+  if (secretFileContent.token_uri !== "https://oauth2.googleapis.com/token") {
+    exitAndLogError(
+        `The Client Secret file at path '${clientSecretPath}' has invalid 'token_uri' value. Expecting value 'https://oauth2.googleapis.com/token', but was '${secretFileContent.token_uri}'. Please make sure you download OAuth client file from GCP Console / API & Services / Credentials.`)
+  }
+
+  if (!secretFileContent.redirect_uris) {
+    exitAndLogError(
+        `The Client Secret file at path '${clientSecretPath}' is missing 'redirect_uris' property. Please make sure you download OAuth client file from GCP Console / API & Services / Credentials.`)
+  }
 }
 
 main().catch(console.error);
